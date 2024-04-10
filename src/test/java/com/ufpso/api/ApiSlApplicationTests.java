3c8cc6f6d78c601176735393b41c46feda4eba2d
laura <ldsepulvedav@ufpso.edu.co>
2024-04-09 19:32:58 -0500

    Creación del proyecto de spring boot con las dependencias necesarias.

---
+++ src/test/java/com/ufpso/api/ApiSlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpso.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
